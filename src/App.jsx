1af46d5511065ed34d3297e8d243b2d80f0ab7ff
Mihir Naik <mdnaik17@gmail.com>
2017-10-26 09:12:20 -0700

    Add and delete button works

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Table extends Component {
-  constructor(props){
-    super(props)
-    console.log(props)
-  }
-
-  addRowClickHandler(evt){
-    console.log("Add button clicked..")
-    console.log(this.refs.Make.value)
-  }
-
-  deleteRowClickHandler(){
-    console.log("Delete butotn clicked..")
-  }
-
-  render(){
-    return (
-      <table className="pure-table pure-table-bordered">
-        <thead>
-          <tr>
-            {this.props.tableData.headers.map(headerName => 
-              <th key={headerName}>{headerName}</th>
-            )}
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {this.props.tableData.headers.map(headerName => 
-              <td>
-                <input ref={headerName} placeholder={headerName}/>
-              </td>
-            )}
-            <td><button className="button-success pure-button" onClick={this.addRowClickHandler.bind(this)}>Add</button></td>
-          </tr>
-          {this.props.tableData.data.map(dataRow => { 
-            return (
-              <tr>
-                {dataRow.map(d => <td>{d}</td>)}
-                <td><button className="button-error pure-button" onClick={this.deleteRowClickHandler.bind(this)}>Delete</button></td>
-              </tr>
-            )}
-          )}
-        </tbody>
-      </table>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     var initialTableData = {
       headers: ["Make", "Model", "Year"],
       data: [['scion', 'box', 2010],
-            ['honda', 'Accord', 2012],
-            ['Chevrolet', 'Camaro', 1980],
+            ['honda', 'accord', 2012],
+            ['chevrolet', 'camaro', 1980],
             ['pontiac', 'firebird', 2014]
           ]
     }
@@ -68,6 +19,84 @@ class App extends Component {
           <Table tableData={initialTableData}/>
       </div>
     );
+  }
+}
+
+class Table extends Component {
+  constructor(props){
+    super(props)
+    this.state = props
+  }
+
+  addRowClickHandler(evt){
+    console.log("Add button clicked..")
+    let values = [this.refs.Make.value, this.refs.Model.value, this.refs.Year.value]
+    this.setState({
+      tableData: {...this.state.tableData, data: [...this.state.tableData.data, values]}
+    })
+  }
+
+  deleteRowClickHandler(row){
+    console.log("Delete button clicked..and deleting", row )
+    let updatedData = this.state.tableData.data.filter((el) =>{
+      return (el[0] !== row[0] && el[1] !== row[1] && el[2] !== row[2])
+    })
+    this.setState({
+      tableData: {...this.state.tableData, data: updatedData}
+    })
+  }
+
+  renderHeaders() {
+    return this.state.tableData.headers.map(headerName => 
+      <td>
+        <input 
+          ref={headerName} 
+          placeholder={headerName}
+        />
+      </td>
+    )
+  }
+
+  render(){
+    return (
+      <table className="pure-table pure-table-bordered">
+        <thead>
+          <tr>
+            {this.state.tableData.headers.map(headerName => 
+              <th key={headerName}>{headerName}</th>
+            )}
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {this.renderHeaders()}
+            <td>
+              <button 
+                className="button-success pure-button" 
+                onClick={this.addRowClickHandler.bind(this)}
+              >
+                Add
+              </button>
+            </td>
+          </tr>
+          {this.state.tableData.data.map(dataRow => { 
+            return (
+              <tr>
+                {dataRow.map(d => <td>{d}</td>)}
+                <td>
+                  <button 
+                    className="button-error pure-button" 
+                    onClick= {this.deleteRowClickHandler.bind(this, dataRow)}
+                    >Delete
+                  </button>
+                </td>
+              </tr>
+            )}
+          )}
+        </tbody>
+      </table>
+    )
   }
 }
 
